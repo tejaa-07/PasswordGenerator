08d17c6db510cd1776a04bd50740d59c2d447640
tejaa-07 <141672086+tejaa-07@users.noreply.github.com>
2023-08-08 00:34:55 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,38 @@
+import random
+import string
+
+def generate_password(length, use_uppercase=True, use_lowercase=True, use_digits=True, use_symbols=True):
+    characters = ''
+
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_lowercase:
+        characters += string.ascii_lowercase
+    if use_digits:
+        characters += string.digits
+    if use_symbols:
+        characters += string.punctuation
+
+    if not characters:
+        print("Error: At least one character type should be selected.")
+        return
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("Password Generator")
+    length = int(input("Enter the desired password length: "))
+
+    use_uppercase = input("Include uppercase letters? (y/n): ").lower() == 'y'
+    use_lowercase = input("Include lowercase letters? (y/n): ").lower() == 'y'
+    use_digits = input("Include digits? (y/n): ").lower() == 'y'
+    use_symbols = input("Include symbols? (y/n): ").lower() == 'y'
+
+    password = generate_password(length, use_uppercase, use_lowercase, use_digits, use_symbols)
+
+    if password:
+        print("Generated Password:", password)
+
+if __name__ == "__main__":
+    main()
